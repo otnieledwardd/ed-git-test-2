8c713fd50497659f17564c303be4b9ae1771881d
Otniel Edward <otnieledwardd@gmail.com:qclearcleargit config --global user.email otnieledwardd@gmail.com>
2022-12-02 13:37:35 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae libero quibusdam at atque vel suscipit, ab architecto deserunt optio pariatur dignissimos expedita ea cupiditate, tempora fugit totam vitae esse iure, quaerat laudantium inventore aut. Corporis animi sint provident hic dicta ipsum non incidunt culpa possimus pariatur dolorem veniam, earum repellendus tempora, vero quos deleniti delectus eius id ad deserunt! Id voluptates sequi suscipit possimus repudiandae reprehenderit aut sapiente sunt recusandae ab natus, soluta alias placeat nemo voluptatibus facere aspernatur officia magni. Consectetur, asperiores ducimus! Voluptates laudantium pariatur iste adipisci quos ipsum, eaque quasi repellendus, deserunt nisi, voluptatum asperiores iure cupiditate.</p>
 </body>
 </html>
